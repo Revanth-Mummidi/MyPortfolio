31e8d266809456d39300969b38616a85a83336b0
Revanth-Mummidi <mummidirevanth@gmail.com>
2024-05-27 15:52:51 +0530

    added cursor animation

--- src/App.js
+++ src/App.js
@@ -8,10 +8,21 @@ import Achievements from "./Components/Achievements";
 import Contact from "./Components/Contact";
 import Profiles from "./Components/Profiles";
 import { Fade } from "react-reveal";
+import AnimatedCursor from "react-animated-cursor";
 
 function App() {
   return (
     <div className="overflow-y-auto no-scrollbar bg-slate-700">
+      <div className="cursor__dot">
+        <AnimatedCursor
+          innerSize={15}
+          outerSize={15}
+          color="255, 255 ,255"
+          outerAlpha={0.4}
+          innerScale={0.7}
+          outerScale={5}
+        />
+      </div>
       <Navbar />
       <HomePage />
       <Fade bottom duration={1500} distance="150px">
